0462f573b0a26efc8b606161a87a3235017bf3d2
yehalex <38019768+yehalex@users.noreply.github.com>
2024-05-27 22:04:22 +0800

    mongodb connected, user can be created and stored (#2)

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import Input from '@/components/input';
 import Image from 'next/image'
+import { signIn } from 'next-auth/react';
 import { useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Auth = () => {
+    const router = useRouter();
     const[email, setEmail] = useState('');
     const[name, setName] = useState('');
     const[password, setPassword] = useState('');
@@ -11,7 +15,36 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
-    }, [])
+    }, []);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials',  {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/',
+            });
+
+            router.push('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, password, router])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password,
+            });
+
+            login();
+        } catch(error) {
+            console.log(error)
+        }
+    }, [email, name, password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -48,7 +81,7 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+                        <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className='text-neutral-500 mt-12'>
