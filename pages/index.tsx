63f4928832ae9fef90a394ec567a3b27b4a93140
Alex PC <alexyeh0911@gmail.com>
2024-05-28 21:10:14 +0800

    Completed Home Page Structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import Navbar from "@/components/Navbar";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <Navbar />
     </>
   );
 }
