840adbf8305ab013d25aeb366da1a7507f415879
Alex PC <alexyeh0911@gmail.com>
2024-05-29 14:23:38 +0800

    Billborad implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import Billboard from "@/components/Billboard";
 import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -23,6 +24,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
