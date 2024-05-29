8b904f3ecdb538bfe15194587b29c1db23274746
yehalex <38019768+yehalex@users.noreply.github.com>
2024-05-29 23:30:50 +0800

    Information Modal added (#6)

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -83,7 +83,7 @@ const Auth = () => {
                         <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
-                        <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                        {/* <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
                             <div 
                                 onClick={() => signIn('google', {callbackUrl: '/profiles'})}
                                 className="
@@ -118,7 +118,7 @@ const Auth = () => {
                             >
                                 <FaGithub size={30}/>
                             </div>
-                        </div>
+                        </div> */}
                         <p className='text-neutral-500 mt-12'>
                             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'} 
                             <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
